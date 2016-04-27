060c2ad48874c90b28e22b6999389e90ee15982a
devanshb <devdocs@gmail.com>
2016-04-27 09:49:56 -0700

    Initial commit

---
+++ Webmvc1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webmvc1.Startup))]
+namespace Webmvc1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
